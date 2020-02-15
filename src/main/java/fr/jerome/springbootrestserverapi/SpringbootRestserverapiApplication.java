8dd4c94ac779de366e4271a25358353311fe71db
jerome <jvk@abes.fr>
2020-02-15 15:05:05 +0100

    FEAT: first commit

---
+++ src/main/java/fr/jerome/springbootrestserverapi/SpringbootRestserverapiApplication.java
@@ -0,0 +1,13 @@
+package fr.jerome.springbootrestserverapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootRestserverapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootRestserverapiApplication.class, args);
+	}
+
+}
