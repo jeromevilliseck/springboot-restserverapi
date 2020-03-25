d7735cc84ada5aae836b9a2a1741f23bb7eecc81
jerome <jvk@abes.fr>
2020-03-25 16:45:52 +0100

    Exception package and classes added

---
+++ src/main/java/fr/jerome/springbootrestserverapi/exception/BusinessRessourceExceptionResponse.java
@@ -0,0 +1,48 @@
+package fr.jerome.springbootrestserverapi.exception;
+
+import org.springframework.http.HttpStatus;
+
+/**
+ * Classe POJO BusinessResourceExceptionResponse de persistance des messages d'erreurs ExceptionResponse
+ */
+public class BusinessRessourceExceptionResponse {
+    private String errorCode;
+    private String errorMessage;
+    private String requestURL;
+    private HttpStatus status;
+
+    public BusinessRessourceExceptionResponse() {
+    }
+
+    public String getErrorCode() {
+        return errorCode;
+    }
+
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    public void setRequestURL(String url) {
+        this.requestURL = url;
+
+    }
+    public String getRequestURL(){
+        return requestURL;
+    }
+
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(HttpStatus status) {
+        this.status = status;
+    }
+}
---
+++ src/main/java/fr/jerome/springbootrestserverapi/exception/GlobalHandlerControllerException.java
@@ -0,0 +1,70 @@
+package fr.jerome.springbootrestserverapi.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice(basePackages = {"fr.jerome.springbootrestserverapi"})
+/*
+Annotation à placer sur la classe qui doit être un controleur
+Permet la gestion globale des exceptions dans un service REST
+Permet d'intercepter et de gérer plusieurs types d'erreurs grâce à l'annotation ExceptionHandler
+
+variable basePackage -> indiquer dans quels packages se trouvent les contrôleurs à prendre en compte par cette classe
+ */
+public class GlobalHandlerControllerException extends ResponseEntityExceptionHandler {
+    @InitBinder
+    /*
+    Annotation permettant une initialisation globale
+     */
+    public void dataBinding(WebDataBinder binder){
+        /*On peut intialiser toute donnée ici, cette classe avec son annotation permet un démarrage automatique
+        au lancement de l'application*/
+    }
+
+    @ModelAttribute
+    /*
+    Permet de créer un objet model global
+    La variable "technicalError" ci-dessous pourra être utilisée n'importe ou dans l'application
+     */
+    public void globalAttributes(Model model){
+        model.addAttribute("technicalError", "Une erreur technique est survenue !");
+    }
+
+    @ExceptionHandler(TechnicalErrorException.class)
+    public ModelAndView technicalErrorException(Exception exception){
+        ModelAndView mav = new ModelAndView();
+        mav.addObject("exception", exception.getMessage());
+        mav.setViewName("error");
+        return mav;
+    }
+
+    @ExceptionHandler(Exception.class)//toutes les autres erreurs non gérées sont interceptées ici
+    //Pour les erreurs techniques non spécifiées, on renvoie vers la vue error.jsp.
+    public ResponseEntity<BusinessRessourceExceptionResponse> unknowError(HttpServletRequest req, Exception ex) {
+        BusinessRessourceExceptionResponse response = new BusinessRessourceExceptionResponse();
+        response.setErrorCode("Technical Error");
+        response.setErrorMessage(ex.getMessage());
+        response.setRequestURL(req.getRequestURL().toString());
+        return new ResponseEntity<BusinessRessourceExceptionResponse>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler(BusinessResourceException.class)
+    public ResponseEntity<BusinessRessourceExceptionResponse> resourceNotFound(HttpServletRequest req, BusinessResourceException ex) {
+        BusinessRessourceExceptionResponse response = new BusinessRessourceExceptionResponse();
+        response.setStatus(ex.getStatus());
+        response.setErrorCode(ex.getErrorCode());
+        response.setErrorMessage(ex.getMessage());
+        response.setRequestURL(req.getRequestURL().toString());
+        return new ResponseEntity<BusinessRessourceExceptionResponse>(response, ex.getStatus());
+    }
+}
